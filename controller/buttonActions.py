c7a21d05b8697a3e4b8e5b6f1e007f66e37e9729
Modesto Amador <mofesdo.dev@gmail.com>
2022-06-14 19:06:14 -0700

    added css code across html pages for consistency

--- controller/buttonActions.py
+++ controller/buttonActions.py
@@ -9,7 +9,7 @@ def loginButtonClicked(usr, pwd):
     if auth is None:
         return "Failed Login"
     else:
-        response = make_response(render_template("dashboard.html"))
+        response = make_response(redirect("/"))
         response.set_cookie("authToken", auth)
         return response
 
